f23aee2bbb31337ff432e9cb35c4ab4c4300ac23
Kopc Blacksmith <keldor@mail.ru>
2016-05-26 15:24:32 +0500

    1197 done

---
+++ src/equpalt/com/vk/WarriorOnField.java
@@ -0,0 +1,245 @@
+package equpalt.com.vk;
+
+import java.util.Scanner;
+
+public class WarriorOnField {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        String str1 = scanner.nextLine();
+
+        while (scanner.hasNextLine()){
+            String s = scanner.nextLine();
+            String sub = s.substring(0,1);
+            String subNum = s.substring(1);
+            switch (sub) {
+                case "a":
+                    switch (subNum) {
+                        case "1":
+                            System.out.println(2);
+                            break;
+                        case "2":
+                            System.out.println(3);
+                            break;
+                        case "3":
+                            System.out.println(4);
+                            break;
+                        case "4":
+                            System.out.println(4);
+                            break;
+                        case "5":
+                            System.out.println(4);
+                            break;
+                        case "6":
+                            System.out.println(4);
+                            break;
+                        case "7":
+                            System.out.println(3);
+                            break;
+                        case "8":
+                            System.out.println(2);
+                            break;
+                    }
+                    break;
+                case "b":
+                    switch (subNum) {
+                        case "1":
+                            System.out.println(3);
+                            break;
+                        case "2":
+                            System.out.println(4);
+                            break;
+                        case "3":
+                            System.out.println(6);
+                            break;
+                        case "4":
+                            System.out.println(6);
+                            break;
+                        case "5":
+                            System.out.println(6);
+                            break;
+                        case "6":
+                            System.out.println(6);
+                            break;
+                        case "7":
+                            System.out.println(4);
+                            break;
+                        case "8":
+                            System.out.println(3);
+                            break;
+                    }
+                    break;
+                case "c":
+                    switch (subNum) {
+                        case "1":
+                            System.out.println(4);
+                            break;
+                        case "2":
+                            System.out.println(6);
+                            break;
+                        case "3":
+                            System.out.println(8);
+                            break;
+                        case "4":
+                            System.out.println(8);
+                            break;
+                        case "5":
+                            System.out.println(8);
+                            break;
+                        case "6":
+                            System.out.println(8);
+                            break;
+                        case "7":
+                            System.out.println(6);
+                            break;
+                        case "8":
+                            System.out.println(4);
+                            break;
+                    }
+                    break;
+                case "d":
+                    switch (subNum) {
+                        case "1":
+                            System.out.println(4);
+                            break;
+                        case "2":
+                            System.out.println(6);
+                            break;
+                        case "3":
+                            System.out.println(8);
+                            break;
+                        case "4":
+                            System.out.println(8);
+                            break;
+                        case "5":
+                            System.out.println(8);
+                            break;
+                        case "6":
+                            System.out.println(8);
+                            break;
+                        case "7":
+                            System.out.println(6);
+                            break;
+                        case "8":
+                            System.out.println(4);
+                            break;
+                    }
+                    break;
+                case "e":
+                    switch (subNum) {
+                        case "1":
+                            System.out.println(4);
+                            break;
+                        case "2":
+                            System.out.println(6);
+                            break;
+                        case "3":
+                            System.out.println(8);
+                            break;
+                        case "4":
+                            System.out.println(8);
+                            break;
+                        case "5":
+                            System.out.println(8);
+                            break;
+                        case "6":
+                            System.out.println(8);
+                            break;
+                        case "7":
+                            System.out.println(6);
+                            break;
+                        case "8":
+                            System.out.println(4);
+                            break;
+                    }
+                    break;
+                case "f":
+                    switch (subNum) {
+                        case "1":
+                            System.out.println(4);
+                            break;
+                        case "2":
+                            System.out.println(6);
+                            break;
+                        case "3":
+                            System.out.println(8);
+                            break;
+                        case "4":
+                            System.out.println(8);
+                            break;
+                        case "5":
+                            System.out.println(8);
+                            break;
+                        case "6":
+                            System.out.println(8);
+                            break;
+                        case "7":
+                            System.out.println(6);
+                            break;
+                        case "8":
+                            System.out.println(4);
+                            break;
+                    }
+                    break;
+                case "g":
+                    switch (subNum) {
+                        case "1":
+                            System.out.println(3);
+                            break;
+                        case "2":
+                            System.out.println(4);
+                            break;
+                        case "3":
+                            System.out.println(6);
+                            break;
+                        case "4":
+                            System.out.println(6);
+                            break;
+                        case "5":
+                            System.out.println(6);
+                            break;
+                        case "6":
+                            System.out.println(6);
+                            break;
+                        case "7":
+                            System.out.println(4);
+                            break;
+                        case "8":
+                            System.out.println(3);
+                            break;
+                    }
+                    break;
+                case "h":
+                    switch (subNum) {
+                        case "1":
+                            System.out.println(2);
+                            break;
+                        case "2":
+                            System.out.println(3);
+                            break;
+                        case "3":
+                            System.out.println(4);
+                            break;
+                        case "4":
+                            System.out.println(4);
+                            break;
+                        case "5":
+                            System.out.println(4);
+                            break;
+                        case "6":
+                            System.out.println(4);
+                            break;
+                        case "7":
+                            System.out.println(3);
+                            break;
+                        case "8":
+                            System.out.println(2);
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+
+}
